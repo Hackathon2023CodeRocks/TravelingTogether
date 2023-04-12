f76c549f1a17b47013a12652711e6a6ee8b4c53d
Hades-des <choennegr@gmail.com>
2023-04-12 19:40:35 +0300

    Заполнение бд

--- app/src/main/java/com/example/travelingtogether/RegisterFragment.java
+++ app/src/main/java/com/example/travelingtogether/RegisterFragment.java
@@ -1,19 +1,68 @@
 package com.example.travelingtogether;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.viewmodel.CreationExtras;
 
 public class RegisterFragment extends Fragment {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private Button registerButton;
+    private DatabaseHelper dbHelper;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view  = inflater.inflate(R.layout.fragment_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        emailEditText = view.findViewById(R.id.emailEditText);
+        passwordEditText = view.findViewById(R.id.passwordEditText);
+        nameEditText = view.findViewById(R.id.nameEditText);
+        registerButton = view.findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+        dbHelper = new DatabaseHelper(getActivity());
         return view;
     }
+
+    private void registerUser() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String name = nameEditText.getText().toString();
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("email", email);
+        values.put("password", password);
+        values.put("name", name);
+        long rowId = database.insert("users", null, values);
+        if (rowId > 0) {
+            Toast.makeText(getActivity(), "User registered successfully", Toast.LENGTH_SHORT).show();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new ProfileFragment());
+            transaction.commit();
+        } else {
+            Toast.makeText(getActivity(), "User registration failed", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
+
+
+
+
