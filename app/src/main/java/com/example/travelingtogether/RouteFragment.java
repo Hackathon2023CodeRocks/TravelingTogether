d938b7ba823a1c2f4e65cc2d2191505eda11a3b1
Hades-des <choennegr@gmail.com>
2023-04-12 21:23:57 +0300

    and some fix

--- app/src/main/java/com/example/travelingtogether/RouteFragment.java
+++ app/src/main/java/com/example/travelingtogether/RouteFragment.java
@@ -2,12 +2,15 @@ package com.example.travelingtogether;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
